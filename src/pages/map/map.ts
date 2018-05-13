26eb5ad027e8e9bd6dd5bf72495d70344c3473e1
Jonathan Miller <network@jonathan-m-miller.solutions>
2018-05-13 12:25:03 -0400

    Adds maps page

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+declare var google;
+
 /**
  * Generated class for the MapPage page.
  *
@@ -15,11 +17,34 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class MapPage {
 
+  map: any;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad MapPage');
+      this.initializeMap();
   }
-
+  initializeMap() {
+ 
+    let locationOptions = {timeout: 20000, enableHighAccuracy: true};
+ 
+    navigator.geolocation.getCurrentPosition(
+ 
+        (position) => {
+ 
+            let options = {
+              center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+              zoom: 16,
+              mapTypeId: google.maps.MapTypeId.ROADMAP
+            }
+ 
+            this.map = new google.maps.Map(document.getElementById("map_canvas"), options);
+        },
+ 
+        (error) => {
+            console.log(error);
+        }, locationOptions
+    );
+}
 }
